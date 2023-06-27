cbf5566caad31eb1562201c75ab5cd1febdd426b
deck0ne <137725065+deck0ne@users.noreply.github.com>
2023-06-26 19:32:05 -0600

    title

---
+++ Final Project/Final Project/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Final_Project
+{
+    internal class Program
+        
+    {
+        static void Title()
+        {
+            Console.WriteLine("                              ********************************************");
+            Console.WriteLine("                              *                                          *");
+            Console.WriteLine("                              *    OBJECT ORIENTED PROGRAM TERM PROJECT  *");
+            Console.WriteLine("                              *        INSTRUCTOR  : Mahbub Murshed      *");
+            Console.WriteLine("                              *         SUBMITTED BY: Dick Fababi        *");
+            Console.WriteLine("                              *                                          *");
+            Console.WriteLine("                              ********************************************");
+        }
+        static void Instruction()
+        {
+            Console.WriteLine("                              ********************************************");
+            Console.WriteLine("                              *                                          *");
+            Console.WriteLine("                              *           CONNECT FOUR PROGRAM           *");
+            Console.WriteLine("                              *                                          *");
+            Console.WriteLine("                              ********************************************");
+        }
+
+        static void Main(string[] args)
+        {
+            Title(); 
+            Instruction();
+        }
+    }
+}
