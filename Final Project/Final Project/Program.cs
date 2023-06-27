cf5ea5c97777d77af7b3bf5e828b2cb51b939b12
deck0ne <137725065+deck0ne@users.noreply.github.com>
2023-06-27 03:45:00 -0600

    lay out the board

--- Final Project/Final Project/Program.cs
+++ Final Project/Final Project/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Final_Project
 {
@@ -23,11 +26,65 @@ namespace Final_Project
             Console.WriteLine("                              *                                          *");
             Console.WriteLine("                              ********************************************");
         }
+       class Connect4
+        {
+            
+            private char [,] GameBoard;
+            private string PlayerName;
 
+            public Connect4()
+            {
+                GameBoard = new char[6, 7];
+                PlayerName = "";
+                Initialize();
+            }
+            private void Initialize()
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        GameBoard[i, j] = ' ';
+                    }
+                }
+            }
+            private void BoardLayout()
+            {
+                Console.Clear();
+
+                for (int i = 0; i < 6; i++)
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        Console.Write(" | " + GameBoard[i, j]);
+                    }
+                    Console.WriteLine(" | ");
+                    Console.WriteLine("---------------");
+                }
+            }
+        }
+       class Player1
+        {
+            public string name;
+        }
+        class Player2:Player1
+        {
+
+        }
         static void Main(string[] args)
         {
-            Title(); 
+
+
+            Title();
             Instruction();
-        }
+            Connect4 p = new Connect4();
+            Console.ReadLine();
+            bool PlayAgain=false;
+            do
+            {
+
+            } while (PlayAgain);
+
+        } 
     }
 }
